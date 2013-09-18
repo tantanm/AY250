ff7da3e150ce182d756b834544dc118b46c6e84e
Sayantan M <sayantanm@Sayantans-MacBook-Pro.local>
2013-09-17 18:55:13 -0700

    setup.py version 1

---
+++ hw2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name = "CalCalc",
+    version = "0.1",
+    script= ['CalCalc.py'],
+    author = "Sayantan Mukhopadhyay",
+    description = "This is an Tiny Package",
+    keywords = "Simple application for Wolfram Alpha"
+)
